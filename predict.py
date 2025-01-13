7e8baa0465e406a50d3f024a27fe4e420abc2612
ttheseus__ <113858167+ttheseus@users.noreply.github.com>
2025-01-13 14:33:59 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,14 +3,19 @@ import os
 from ultralytics import YOLO
 import cv2
 
-VIDEOS_DIR = os.path.join('.', 'videos')
+# VIDEOS_DIR = os.path.join('home', 'user', 'Documents', 'ai', 'yolo_testai2-main', 'video')
 
-video_path = os.path.join(VIDEOS_DIR, '[video name]')
-video_path_out = '{}_out.mp4'.format(video_path)
+video_path = os.path.join('/', 'home', 'user', 'Documents', 'ai', 'yolo_testai2-main', 'video', 'VIDEO.mp4') #/home/user/Documents/ai/yolo_testai2-main/VIDEO_out.avi'
+if not os.path.exists(video_path):
+    print(f"Error: Video file does not exist at {video_path}")
+else:
+    print(f"Video file found at {video_path}")
 
-cap = cv2.VideoCapture(video_path)
+video_path_out = '{}_out.avi'.format(os.path.splitext(video_path)[0])
+
+cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
 if not cap.isOpened():
-    print("Error: Could not open video.")
+    print("Error: Could not open video at {video_path}.")
     exit()
 
 
@@ -20,13 +25,13 @@ if not ret or frame is None:
     exit()
 
 H, W, _ = frame.shape
-out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MJPG'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'last.pt')
+model_path = os.path.join('/', 'home', 'user', 'runs', 'detect', 'train19', 'weights', 'last.pt')
 
 model = YOLO(model_path)
 
-threshold = 0.5
+threshold = 0.3
 
 while ret:
 
@@ -36,7 +41,7 @@ while ret:
         x1, y1, x2, y2, score, class_id = result
         
         if score > threshold:
-            cv2.rectangle(frame, (int(x1)), (int(y1)), (int(x2)), (int(y2)), (0, 255, 0), 4)
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,255,0), 3, cv2.LINE_AA)
     
     out.write(frame)
