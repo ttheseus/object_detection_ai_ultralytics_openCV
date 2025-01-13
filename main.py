621a909746da1c6950cbebcaf78f530b42b93a58
ttheseus__ <113858167+ttheseus@users.noreply.github.com>
2025-01-13 09:50:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------# 
+
+ #### ####  :*-_-*-_-*-_-*- -  
+########### | [ minecraft mob detection AI - training code]  
+ #########  |    -> dorothy
+   #####    |                                                      
+     #      :*-_-*-_-*-_-*- -
+   
+    #| useful video follow along for the code that i used: 
+    #| https://www.youtube.com/watch?v=Z-65nqxUdl4&t=1979s
+    #|
+    #| go into terminal and enter 'pip install ultralytics'
+
+#-------------------------------------------------------------#
+from ultralytics import YOLO 
+
+# Load a model
+model = YOLO("yolov8n.pt") # build model from scratch
+
+results = model.train(data="config.yaml", epochs=3) #train the model
