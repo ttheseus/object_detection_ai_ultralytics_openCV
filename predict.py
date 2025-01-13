203feb137bb623197e798224f780ea2e6aacb925
ttheseus__ <113858167+ttheseus@users.noreply.github.com>
2025-01-13 13:38:36 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -9,12 +9,16 @@ video_path = os.path.join(VIDEOS_DIR, '[video name]')
 video_path_out = '{}_out.mp4'.format(video_path)
 
 cap = cv2.VideoCapture(video_path)
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-            assert not isinstance(frame,type(None)), 'frame not found'
+if not cap.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
 
 ret, frame = cap.read()
+if not ret or frame is None:
+    print("Error: Could not read the first frame.")
+    exit()
+
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
@@ -32,11 +36,16 @@ while ret:
         x1, y1, x2, y2, score, class_id = result
         
         if score > threshold:
-            cv2.rectangle(fram, (int(x1)), (int(y1)), (int(x2)), (int(y2)), (0, 255, 0), 4)
+            cv2.rectangle(frame, (int(x1)), (int(y1)), (int(x2)), (int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,255,0), 3, cv2.LINE_AA)
     
     out.write(frame)
     ret, frame = cap.read()
+
+    if ret and frame is not None:
+        H, W, _ = frame.shape
+    else:
+        break
 
 cap.release()
 out.release()
